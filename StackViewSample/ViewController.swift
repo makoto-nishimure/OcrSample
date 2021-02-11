294f9f6292a45dbcaf8aaa48a168c088530a5ade
makoto.nishimure <tkm.nishimure@gmail.com>
2021-02-11 23:40:43 +0900

    TableView, TableViewCellの実装

--- StackViewSample/ViewController.swift
+++ StackViewSample/ViewController.swift
@@ -7,12 +7,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+
+    let TODO = ["境界線上のホライゾン", "Heart of Lapis", "芥見下々"]
+    let CTX = ["Horizon on the Middle of Nowhere", "ラピスの心臓", "呪術廻戦"]
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return TODO.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        print("tableView: \(indexPath.row)")
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cells", for: indexPath) as! TableCell
+        tableView.rowHeight = 80
+        cell.title?.text = TODO[indexPath.row]
+        cell.context?.text = CTX[indexPath.row]
+        return cell
+    }
+    
+
 
 
 }
---
+++ TableCell.swift
@@ -0,0 +1,13 @@
+//
+//  TableCell.swift
+//  StackViewSample
+//
+//  Created by makoto on 2021/02/11.
+//
+
+import UIKit
+
+class TableCell:UITableViewCell {
+    @IBOutlet weak var title:UILabel!
+    @IBOutlet weak var context: UILabel!
+}
